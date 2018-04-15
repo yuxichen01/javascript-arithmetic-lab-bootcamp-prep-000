a260aa3fae8448e8f44a57c41d926fda526d215f
yuxichen11@gmail.com <yuxichen11@gmail.com>
2018-04-15 04:44:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
-
+function add(a,b) {
+  return a += b ;
+}
+function subtract(a,b) {
+  return a -= b; 
+}
+function multiply(a,b) {
+  return a *= b ;
+}
+function divide(a,b) {
+  return a /= b ;
+}
+function inc(n) {
+  n++;
+  return n;
+}
+function dec(n) {
+  n--;
+  return n;
+}
 function makeInt(n) {
- if parseInt(n,10)
+ parseInt(n,10)
  return n 
 } else if {
  (n === '0x2328') 
